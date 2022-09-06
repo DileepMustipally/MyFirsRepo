5ec007bde14afc8dedd795ba34584aec13d45c00
DileepMustipally <dileepreddy1495@gmail.com>
2022-09-06 12:49:14 +1000

    MySecondRepo

---
+++ Public.java
@@ -0,0 +1,3 @@
+public class Public {
+    public static void main
+}
---
+++ src/main/java/AutomationDemo/Automation.java
@@ -0,0 +1,7 @@
+package AutomationDemo;
+
+public class Automation {
+    public static void main(String[] args) {
+System.out.println("this is my first program");
+    }
+}
